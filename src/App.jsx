c9e10e0be44600a6f061ce5c04a31ad8ab201b4c
Ange-Pascal <angeakoumoua@gmail.com>
2023-07-03 11:13:46 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import PostsList from "./components/PostsList"; 
+import MainHeader from "./components/MainHeader";
+
+function App() { 
+
+  const [modalIsVisible, setModalIsVisible] = useState(false);   
+  function showModalHandler(){
+    setModalIsVisible(true);
+  }
+  function hideModalHandler(){
+    setModalIsVisible(false);
+  }
+  return ( 
+    <>
+    <MainHeader onCreatePost={showModalHandler}/>
+    <main>
+      <PostsList isPoting={modalIsVisible} onStopPosting={hideModalHandler}/>
+    </main> 
+    </>
+  );
+}
+
+export default App;
